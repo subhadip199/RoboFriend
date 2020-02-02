2e4de352f1a62b4e62023518d0777b148b82ac42
subhadip nandi <48771530+subhadip199@users.noreply.github.com>
2020-02-02 18:26:16 +0530

    adding project 2nd time

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const scroll=(props)=>{
-	return(
-		<div style={{overflow:'scroll',border: '5px solid black',height:'800px'}}>
-				{props.children}		
-		</div>
-		)
+const Scroll = (props) => {
+  return (
+    <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+      {props.children}
+    </div>
+  );
 };
-export default scroll;
+
+export default Scroll;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,15 @@
 import React from 'react';
-const SearchBox=({searchchange})=>{
+
+
+
+const SearchBox=({searchChange})=>{
 	return(
 		<div className='pa2'>
 			<input
 				className='pa3 ba b--green bg-lightest-blue'
 				type='search'
 				placeholder='search robots'
-				onchange={searchchange}
+				onChange={searchChange}
 			 />
 		</div>
 
